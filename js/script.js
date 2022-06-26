bb686b8c623592add61c855d1b2e5e0cd66223b4
alom77 <78904299+alom77@users.noreply.github.com>
2022-06-26 22:39:50 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+$('#one').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+    
+})
+
+$('#two').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+    
+})
+
+$('#three').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2.5
+        },
+        600:{
+            items:3.5
+        },
+        1000:{
+            items:4
+        }
+    }
+    
+})
+
